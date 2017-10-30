29b6509bb737c602302d80b22bf88a9d578b81c1
secret_online <me@secretonline.co>
2017-10-31 09:59:38 +1300

    Add config read/write

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const pkg = require('./package.json');
+
+const client = new Discord.Client();
+
+function readConfig() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./config.json', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  })
+    .then(JSON.parse);
+}
+
+function writeConfig(conf) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./config.json', JSON.stringify(conf, null, 2), (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+}
+
+readConfig()
+  .then((config) => {
+    client.login(config.token);
+  }, (err) => {
+    console.error('unable to read config');
+    console.error(err);
+  });
